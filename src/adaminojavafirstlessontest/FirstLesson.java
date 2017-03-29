5d1b121aa2e1729bbeae93879c18c326b18a7f07
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2017-03-29 13:55:24 +0200

    Firsts Lesson

--- src/adaminojavafirstlessontest/FirstLesson.java
+++ src/adaminojavafirstlessontest/FirstLesson.java
@@ -5,29 +5,84 @@
  */
 package adaminojavafirstlessontest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FirstLesson {
 
     //Create field to hold studentFirstName, initialize it to your first name
     //Create field to hold the value isLearning, initialize it to true
     //Create field to hold an int called currentSemester, initialize it to 2
     //Create a public constant field to hold a name called TEACHER, initialize to Adam
+    private String studentFirstName;
+    private boolean isLearning;
+    private int currentSemester;
+    public static final String TEACHER = "Adam";
+    int[] arrayOfSemesters = {1, 2, 3, 4, 5};
+    private List<Integer> arrayListOfSemesters;
+
     public FirstLesson() {
+        studentFirstName = "Mathias";
+        isLearning = true;
+        currentSemester = 2;
     }
 
     //Create a constructor which can studentFirstName as argument and sets the value of isLearning to false and currentSemester to 1
+    public FirstLesson(String studentFirstName) {
+        this.studentFirstName = studentFirstName;
+        isLearning = false;
+        currentSemester = 1;
+        arrayListOfSemesters = new ArrayList<>();
+    }
+
     //Create a method to print studentName to the console called printStudentName
+    public void printStudentName() {
+        System.out.println(studentFirstName);
+    }
+
     //Create a method to print studentName if the value isLearning is true, call it printLearningStudent
+    public void printLearningStudent() {
+        if (isLearning) {
+            printLearningStudent();
+        }
+    }
+
     /*Create a method that takes an int "semester" as argument
     and returns studentName in case the int equals 2 and returns an empty value in case semester is 1, 3, 4, 5 or default
     Call it returnOnSecondSemester
      */
- /*Create an int Array field which contains numbers from 1 to 5,
-call it arrayOfSemesters
-remember to create a getter for it afterwards!
+    public String returnOnSecondSemester(int semester) {
+        if (semester == 2) {
+            return studentFirstName;
+        } else {
+            return "";
+        }
+    }
+
+    /*Create an int Array field which contains numbers from 1 to 5,
+    call it arrayOfSemesters
+    remember to create a getter for it afterwards!
      */
- /*Create an Integer ArrayList field that also contains the numbers from 1 to 5,
-call it arrayListOfSemesters
-remember to create a getter for it afterwards*/
+    public int[] getArrayOfSemesters() {
+        return arrayOfSemesters;
+    }
+
+    /*Create an Integer ArrayList field that also contains the numbers from 1 to 5,
+    call it arrayListOfSemesters
+    remember to create a getter for it afterwards
+     */
+    public List<Integer> getArrayListOfSemesters() {
+        return arrayListOfSemesters;
+    }
+
     //Create a method to set the value isLearning to a new value
+    public void setIsLearning(boolean isLearning) {
+        this.isLearning = isLearning;
+    }
+
     //Create methods to get all the fields from this class
+    //          ?!?!?!?!?!?!?!?!??!?!?!?!
+    public FirstLesson getFirstLesson() {
+        return this;
+    }
 }
